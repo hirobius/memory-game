1b8870c9cf93bea3a96ac9541364b730dd3d653b
arnone215 <tarnone21589@gmail.com>
2021-03-03 21:15:43 -0800

    JS function to-do list created

---
+++ js/app.js
@@ -0,0 +1,34 @@
+'use strict';
+
+// Functions to Create
+
+// 1. Global Variables
+// --- empty card array
+let emptyCard = [];
+// --- leaderboard array
+let topTen = [];
+// --- click counter
+let clicks = [];
+// --- timer
+// --- reset button
+
+// 2. Create board function (rendering board)
+// - click - play button
+
+// 3. Arrange card function
+// - click - easy/med/hard
+
+// 4. Flip card function
+// - click - card flip
+
+// 5. Check match/equiality function
+// - IF INCORRECT, flip back over
+// - remove click - card flip (IF MATCHED)
+
+// 6. MODAL play again function
+// - click - play button
+
+// 7. Render to leaderboard IF IN TOP 10
+// --- congratulations notification
+// --- sort scores from HIGH/LOW
+
